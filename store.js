ba6461822adf4dd2b5761d240ebe8c9c451ac81a
Sophie Janson <sophie@sophiejanson.com>
2018-05-29 15:06:24 +0200

    added shopping functionality and cart display

--- store.js
+++ store.js
@@ -11,33 +11,48 @@ var products = [
   { referenceNumber: 1238, name: "Light On Yoga", category: "misc", price: 10 }
 ]
 // Declare `shoppingCart`, something where you will be storing all products that the user buys.
-let shoppingCart;
+let shoppingCart = [];
+let totalPrice = 0;
 
 // Declare `products`, the different that you will be selling under each of the departments.
-
-var shopFromStore =  function () {
-  var refNr = askUserForReferenceNumber();
+var shopFromStore = function (referenceNumber) {
   // Add the product with the matching referenceNumber to the shoppingCart
 
-  displayProductsFromShoppingCart();
+  var shop = function() {
+    let price = products.find(product => product.referenceNumber === referenceNumber).price;
+    let newProduct = {referenceNumber: referenceNumber, price: price};
+    shoppingCart.push(newProduct);
+    totalPrice += parseInt(newProduct.price);
+    displayTotalPrice(totalPrice);
+    displayProductsFromShoppingCart(newProduct);
+  }
 
+  shop();
   // calculate the total price of your cart, and use it:
-  displayTotalPrice(/*The variable holding the totol price*/);
-
+}
   // Ask the user if they want to continue shopping,
   // if yes, call this function again
   // if no, print the goodbye message
-};
+var displayProductsFromShoppingCart = function(newProduct) {
+  let product = newProduct;
+  var referenceNumberElement = document.createElement('span');
+  referenceNumberElement.className  = 'referenceNumber';
+  referenceNumberElement.innerText = product.referenceNumber;
 
-var displayProductsFromShoppingCart = function() {
-  // iterate over the shoppingCart and display the contents
+  var priceElement = document.createElement('span');
+  priceElement.className  = 'price';
+  priceElement.innerText = product.price;
 
+  var productElement = document.createElement('div');
+  productElement.className = 'product';
+  productElement.setAttribute('data-ref-number', product.referenceNumber);
+
+  productElement.appendChild(referenceNumberElement);
+  productElement.appendChild(priceElement);
+
+  // Hang that div on the page
+  document.getElementById('shopping-cart').appendChild(productElement);
   // use the printProductsOnScreen function for inspiration
-};
-
-var askUserForReferenceNumber = function() {
-  // Use window.prompt to ask the user a question and capture their response,
-  // then, return the response from this function back to our caller
 };
 
 //
@@ -60,29 +75,43 @@ var printProductsOnScreen = function () {
     nameElement.className  = 'name';
     nameElement.innerText = product.name;
 
+    var categoryElement = document.createElement('span');
+    categoryElement.className  = 'category';
+    categoryElement.innerText = product.category;
+
     var priceElement = document.createElement('span');
     priceElement.className  = 'price';
     priceElement.innerText = product.price;
 
+
     // Wrap our just created elements in a div
     var productElement = document.createElement('div');
     productElement.className = 'product';
+    productElement.setAttribute('data-ref-number', product.referenceNumber);
 
     productElement.appendChild(referenceNumberElement);
     productElement.appendChild(nameElement);
+    productElement.appendChild(categoryElement);
     productElement.appendChild(priceElement);
 
     // Hang that div on the page
     document.getElementById('product-overview').appendChild(productElement);
+  }
+};
 
-
+var createProductEventListeners = function() {
+  let products = document.querySelectorAll('div[data-ref-number]');
+  console.log(products);
+  for(var product of products) {
+    product.addEventListener("click", function(){
+      shopFromStore(parseInt(this.dataset.refNumber));
+    });
   }
 };
 
 var runApp = function () {
   printProductsOnScreen();
-
-  shopFromStore();
+  createProductEventListeners();
 };
 
 document.onreadystatechange = function () {
