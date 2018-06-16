3b18cd4559dc950076e065656e97f799113e054f
Sophie Janson <sophie@sophiejanson.com>
2018-06-16 16:26:37 +0200

    Added possibility to win or lise

--- week3/homework/hangman/src/components/WordContainer.js
+++ week3/homework/hangman/src/components/WordContainer.js
@@ -15,8 +15,27 @@ class WordContainer extends React.PureComponent {
     return gameLogic.showGuess(this.props.randomWord, this.props.guesses)
   }
 
+  isWinner = () => {
+    return gameLogic.isWinner(this.props.randomWord, this.props.guesses)
+  }
+
+  gameFinished = () => {
+    return gameLogic.gameFinished(this.props.randomWord, this.props.guesses)
+  }
+
+  evaluateGameEndResult = () => {
+    return gameLogic.isWinner(this.props.randomWord, this.props.guesses) ? 'Woohoo, you are amazing! You Won!' : 'You lost.'
+  }
+
   render() {
-    if (!this.props.randomWord) return 'Getting words from the word universe...'
+    if (!this.props.randomWord) return <h2>'Getting words from the word universe...'</h2>
+
+    if(gameLogic.gameFinished(this.props.randomWord, this.props.guesses)) {
+      return (
+        <h2>{this.evaluateGameEndResult()}</h2>
+      )
+    }
+
     return <Word randomWord={this.hideWord()} />
   }
 }
